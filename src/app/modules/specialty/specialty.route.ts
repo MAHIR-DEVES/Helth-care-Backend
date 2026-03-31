b3cfcc53a42161f148acb3124011272733130a7a
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-03-31 13:00:03 +0600

    part 4 complete

--- src/app/modules/specialty/specialty.route.ts
+++ src/app/modules/specialty/specialty.route.ts
@@ -1,14 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from 'express';
 import { specialtyController } from './specialty.controller';
 import { checkAuth } from '../../middleware/checkAuth';
 import { Role } from '../../../generated/prisma/enums';
+import { multerUpload } from '../../config/multer.config';
+import { validateRequest } from '../../middleware/validateRequest';
+import { SpecialtyValidation } from './specialty.validation';
 
 const router = Router();
 
 router.post(
   '/',
-  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  // checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single('file'),
+  validateRequest(SpecialtyValidation.createSpecialtyZodSchema),
   specialtyController.createSpecialty,
 );
 router.get('/', specialtyController.getAllSpecialty);
